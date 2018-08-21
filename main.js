837e5acc3c61d4a7f9ed66203e63cbeb0f065e8a
Prashant Rajoriya <prashant.rajoriya21@gmail.com>
2018-08-21 13:24:48 +0530

    Update corrected navbar to fixed and width of main-doc

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ let main = document.getElementById('main-doc');
 
 menu.addEventListener('click', () => {
     navbar.style.display = 'flex';
+    main.classList.add('mainChange');
 });
 
 close.addEventListener('click', () => {
     navbar.style.display = 'none';
+    main.classList.remove('mainChange');
 });
